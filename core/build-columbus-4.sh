fc9b7a8c2e63cf3ce4b25c4a2686ef79e50c8ec5
Jade Kim <jade@buzlink.io>
2020-10-21 12:16:51 +0900

    Modify tag

--- core/build-columbus-4.sh
+++ core/build-columbus-4.sh
@@ -5,7 +5,7 @@ CHAIN_ID="columbus-4"
 TAG="$CHAIN_ID"
 GENESIS='https://columbus-genesis.s3-ap-northeast-1.amazonaws.com/columbus-4-genesis.json'
 
-docker build -t terra-core:$TAG \
+docker build -t buzlinklabs/terra-core:$TAG \
     --build-arg TERRA_VERSION="$TERRA_VERSION" \
     --build-arg GENESIS="$GENESIS" \
     .
--- core/build-tequila-0004.sh
+++ core/build-tequila-0004.sh
@@ -5,7 +5,7 @@ CHAIN_ID="tequila-0004"
 TAG="$CHAIN_ID"
 GENESIS='https://raw.githubusercontent.com/terra-project/testnet/master/tequila-0004/genesis.json'
 
-docker build -t terra-core:$TAG \
+docker build -t buzlinklabs/terra-core:$TAG \
     --build-arg TERRA_VERSION="$TERRA_VERSION" \
     --build-arg GENESIS="$GENESIS" \
     .
