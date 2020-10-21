27945781e0d3ca52d0b289436b198dddef269798
Jade Kim <jade@buzlink.io>
2020-10-21 17:59:18 +0900

    Add snapshot target height

--- core-snapshot/tequila-0004.sh
+++ core-snapshot/tequila-0004.sh
@@ -1,4 +1,5 @@
 docker build -t buzlinklabs/terra-core:tequila-0004-snapshot \
+    --build-arg TARGET_HEIGHT=$1 \
     --build-arg CHAIN_ID=tequila-0004 \
     --build-arg ENV_MONIKER=terra-docker \
     --build-arg ENV_MIN_GAS_PRICES=0.15uluna,0.15uusd,0.1018usdr,178.05ukrw,431.6259umnt \
--- core-snapshot/wait-sync.sh
+++ core-snapshot/wait-sync.sh
@@ -1,11 +1,48 @@
 #! /bin/sh
 
-function is_syncing() {
-    return "$(curl -s -G http://localhost:1317/syncing)" = '{"syncing":true}'
+is_syncing() {
+    echo "$(curl -s -G http://localhost:1317/syncing)" = '{"syncing":true}'
 }
 
-while [ is_syncing ]
+timestamp() {
+    echo $(date +%s)
+}
+
+last_commit_time=$(timestamp)
+
+check_sync() {
+    if [ $(( $last_commit_time + 10 )) -lt $(timestamp) ]
+    then
+        if [ ! $(is_syncing) ]
+        then
+            exit 0
+        fi
+    fi
+}
+
+height=""
+
+while read line
 do
-    echo $(jobs -p)
-    sleep 10;
+    height="$(echo $line | sed -n 's/^.*Committed.*height=\([[:digit:]]\+\).*$/\1/p')"
+
+    if [ -z "$height" ]
+    then
+        $(check_sync)
+    else
+        last_commit_time=$(timestamp)
+        echo "synced $height"
+
+        if [ $# -lt 1 ] || [ $1 -lt 1 ]
+        then
+            $(check_sync)
+            continue
+        fi
+
+        if [ $height -gt $1 ]
+        then
+            echo "Reach limit ($1/$height)"
+            exit 0
+        fi
+    fi
 done
